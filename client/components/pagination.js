3981eab51379583d2dbfa5c890ae70449c052c6e
Marina <patamga@mail.ru>
2020-11-08 11:34:52 +0300

    fix

--- client/components/pagination.js
+++ client/components/pagination.js
@@ -30,7 +30,7 @@ const Pagination = (props) => {
       <button
         type="button"
         onClick={() => dispatch(setSkip(props.skip + props.limit))}
-        className=" px-3 py-1 mr-10 border-2 border-gray-400 bg-gray-100 hover:bg-white text-blue-700 hover:text-blue-500"
+        className=" px-4 py-1 mr-10 border-2 border-gray-400 disabled:opacity-50 bg-gray-100 hover:bg-white text-blue-700 hover:text-blue-500"
         disabled={props.skip >= props.countIssues - props.limit}
       >
         Next
@@ -40,7 +40,7 @@ const Pagination = (props) => {
         type="button"
         onClick={() => dispatch(setSkip(props.countIssues - props.limit))}
         disabled={props.skip >= props.countIssues - props.limit}
-        className=" px-4 py-1 mr-10 border-2 border-gray-400 disabled:opacity-50 bg-gray-100 hover:bg-white text-blue-700 hover:text-blue-500 "
+        className=" px-4 py-1 mr-10 border-2 border-gray-400 disabled:opacity-50 bg-gray-100 hover:bg-white text-blue-700 hover:text-blue-500"
       >
         End
       </button>
