8b1730541fb6cfaf757eed8a58aa8c5f2bcaa94e
Marina <patamga@mail.ru>
2020-11-08 11:20:13 +0300

    pagination

--- client/components/header.js
+++ client/components/header.js
@@ -17,36 +17,38 @@ const Pagination = (props) => {
         Total question: <span className="text-gray-900 font-bold">{props.countIssues}</span>
       </div>
 
-      <div>
-        Show questions per page
-        <button
-          type="button"
-          onClick={() => dispatch(setLimit(5))}
-          className=" px-3   text-blue-700 hover:text-blue-500"
-        >
-          5
-        </button>
-        <button
-          type="button"
-          onClick={() => dispatch(setLimit(10))}
-          className=" px-3   text-blue-700 hover:text-blue-500"
-        >
-          10
-        </button>
-        <button
-          type="button"
-          onClick={() => dispatch(setLimit(20))}
-          className=" px-3 text-blue-700 hover:text-blue-500"
-        >
-          20
-        </button>
-        <button
-          type="button"
-          onClick={() => dispatch(setLimit(30))}
-          className=" px-3 text-blue-700 hover:text-blue-500"
-        >
-          30
-        </button>
+      <div className="px-4 flex">
+        <div clasName="">Show per page</div>
+        <div clasName="">
+          <button
+            type="button"
+            onClick={() => dispatch(setLimit(5))}
+            className=" px-1   text-blue-700 hover:text-blue-500"
+          >
+            5
+          </button>
+          <button
+            type="button"
+            onClick={() => dispatch(setLimit(10))}
+            className=" px-1   text-blue-700 hover:text-blue-500"
+          >
+            10
+          </button>
+          <button
+            type="button"
+            onClick={() => dispatch(setLimit(20))}
+            className=" px-1 text-blue-700 hover:text-blue-500"
+          >
+            20
+          </button>
+          <button
+            type="button"
+            onClick={() => dispatch(setLimit(30))}
+            className=" px-1 text-blue-700 hover:text-blue-500"
+          >
+            30
+          </button>
+        </div>
       </div>
     </div>
   )
--- client/redux/reducers/index.js
+++ client/redux/reducers/index.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux'
 import { connectRouter } from 'connected-react-router'
 import question from './question'
 
-
 const createRootReducer = (history) =>
   combineReducers({
     router: connectRouter(history),
